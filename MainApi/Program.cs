00c5c0318dd73a82e95cf0feb0fef992ab1a5d84
Alfonso M. Evangelista <a03.evangelista@gmail.com>
2025-08-14 23:18:39 -0400

    add db conn

--- MainApi/Program.cs
+++ MainApi/Program.cs
@@ -1,15 +1,18 @@
+using Dapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.MapGet("/users", () =>
 {
-    var users = new User[]{new (Guid.CreateVersion7(), "alfonso"),
-        new (Guid.CreateVersion7(), "maritza"),
-        new (Guid.CreateVersion7(), "arlette") };
-    return users;
+    var connectionString = Environment.GetEnvironmentVariable("CONN");
+    using var con = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+    con.Open();
+
+    return con.Query<User>("SELECT * FROM users");
 });
 
 app.Run();
 
-internal record User(Guid Id, string UserName);
+internal record User(int Id, string UserName);
